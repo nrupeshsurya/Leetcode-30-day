95d7cc17c97a33ee2aff7fc74af2b53999d96842
Nrupesh Surya <50907680+nrupeshsurya@users.noreply.github.com>
2020-04-07 08:12:07 +0530

    Update q6 Anagram.cpp

--- q6 Anagram.cpp
+++ q6 Anagram.cpp
@@ -17,55 +17,44 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int main()
-{
-    vector <pair<string,int>> a;
-    for(int i=0;i<6;i++)
-    {
-        string p;
-        cin>>p;
-        a.push_back({p,i});
-    }
-    vector <string> temp;
-    for (int i = 0; i < 6; i++)
-    {
-        temp.push_back(a[i].first);
-    }
-    for(int i=0;i<6;i++)
-    {
-        sort(a[i].first.begin(),a[i].first.end());
-    }
-    sort(a.begin(),a.end());
-    vector<vector<string>> ans;
-    int k=0;
-    vector<string> ans2;
-    ans2.push_back(temp[a[0].second]);
-    for(int i=1;i<6;i++)
-    {
-        if(a[i].first==a[i-1].first)
+
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        vector<pair<string,int>> a;
+        for(int i=0;i<strs.size();i++)
         {
-            ans2.push_back(temp[a[i].second]);
+            a.push_back({strs[i],i});
         }
-        else
+        for(int i=0;i<a.size();i++)
+        {
+            sort(a[i].first.begin(),a[i].first.end());
+        }
+        sort(a.begin(),a.end());
+        vector<vector<string>> ans;
+        int k=0;
+        vector <string> ans2;
+        ans2.push_back(strs[a[0].second]);
+        for(int i=1;i<a.size();i++)
+        {
+            if(a[i].first==a[i-1].first)
+            {
+                ans2.push_back(strs[a[i].second]);
+            }
+            else
+            {
+                k++;
+                ans.push_back(ans2);
+                ans2.clear();
+                ans2.push_back(strs[a[i].second]);
+            }
+        }
+        if(ans2.size()>0)
         {
             k++;
             ans.push_back(ans2);
-            ans2.clear();
-            ans2.push_back(temp[a[i].second]);
         }
+        return ans;
     }
-    if(ans2.size()>0)
-    {
-        ans.push_back(ans2);
-        k++;
-    }   
-    for(int i=0;i<=k;i++)
-    {
-        for(int j=0;j<ans[i].size();j++)
-        {
-            cout<<ans[i][j]<<" ";
-        }
-        cout<<"\n";
-    }
-    return 0;
-}
+};
+
