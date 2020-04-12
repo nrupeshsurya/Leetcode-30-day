3634b458b8da5ad7c428206ed8db4a7f2ef096c4
Nrupesh Surya <nrupeshsurya@gmail.com>
2020-04-12 15:39:17 +0530

    update q11

--- q11 Diameter of Binary Tree.cpp
+++ q11 Diameter of Binary Tree.cpp
@@ -1,15 +1,13 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
- * };
- */
+
+  struct TreeNode {
+      int val;
+      TreeNode *left;
+      TreeNode *right;
+      TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+  };
 class Solution {
 public:
     int ans = 0;
