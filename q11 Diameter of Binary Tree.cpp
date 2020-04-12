3afa36f79cad817f848b2da46d0851b57177f619
Nrupesh Surya <nrupeshsurya@gmail.com>
2020-04-12 15:37:46 +0530

    q11 diameter of a binary tree(dfs)

---
+++ q11 Diameter of Binary Tree.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    int ans = 0;
+    int heightOfBinaryTree(TreeNode* root)
+    {
+        if(root==NULL)
+        {
+            return 0;
+        }
+        int left = heightOfBinaryTree(root->left);
+        int right = heightOfBinaryTree(root->right);
+        ans = max(ans,left+right);
+        return 1+max(left,right);
+    }
+    int diameterOfBinaryTree(TreeNode* root) {
+        heightOfBinaryTree(root);
+        return ans;
+    }
+};
